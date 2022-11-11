a9fc96baea174daee0bc06018987963e30d50659
Arūnas Zamalis <8914148+uwubeb@users.noreply.github.com>
2022-11-11 11:37:54 +0200

    auth attributes

--- RestoranuValdymoSistema/RestoranuValdymoSistema/Program.cs
+++ RestoranuValdymoSistema/RestoranuValdymoSistema/Program.cs
@@ -132,7 +132,7 @@ app.MapPut("/restaurants", [Authorize(Roles = "admin")] async (UpdateRestaurantC
     return Results.NoContent();
 });
 
-app.MapDelete("/restaurants/{id}", [Authorize] async (Guid id, IRestaurantRepository repo) =>
+app.MapDelete("/restaurants/{id}", [Authorize(Roles = "admin")] async (Guid id, IRestaurantRepository repo) =>
 {
     await repo.Delete(id);
 
@@ -141,23 +141,23 @@ app.MapDelete("/restaurants/{id}", [Authorize] async (Guid id, IRestaurantReposi
 });
 
 // Orders
-app.MapGet("/restaurants/{restaurantId}/orders",
-    async (IOrderRepository repo, Guid restaurantId, IMapper mapper) =>
+app.MapGet("/restaurants/{restaurantId}/orders", [Authorize(Roles = "user, admin")]
+async (IOrderRepository repo, Guid restaurantId, IMapper mapper) =>
 {
     var orders = await repo.Get(restaurantId);
     var orderContracts = mapper.Map<List<OrderContract>>(orders);
     return Results.Ok(orderContracts);
 });
 
-app.MapGet("/restaurants/{restaurantId}/orders/{orderId}",
-    async (Guid orderId, Guid restaurantId, IOrderRepository repo, IMapper mapper) =>
+app.MapGet("/restaurants/{restaurantId}/orders/{orderId}", [Authorize(Roles = "user, admin")]
+async (Guid orderId, Guid restaurantId, IOrderRepository repo, IMapper mapper) =>
 {
     var order = await repo.Get(restaurantId, orderId);
     var orderContract = mapper.Map<OrderContract>(order);
     return Results.Ok(orderContract);
 });
 
-app.MapPost("/restaurants/{restaurantId}/orders", async (CreateOrderContract orderContract, Guid restaurantId, IOrderRepository repo, IMapper mapper) =>
+app.MapPost("/restaurants/{restaurantId}/orders", [Authorize(Roles = "admin")] async (CreateOrderContract orderContract, Guid restaurantId, IOrderRepository repo, IMapper mapper) =>
 {
     var order = mapper.Map<Order>(orderContract);
     await repo.Create(restaurantId, order);
@@ -165,36 +165,36 @@ app.MapPost("/restaurants/{restaurantId}/orders", async (CreateOrderContract ord
     return Results.Created($"/restaurants/{restaurantId}/orders/{orderContractResult.Id}", orderContractResult);
 });
 
-app.MapPut("/restaurants/{restaurantId}/orders", async (UpdateOrderContract orderContract, Guid restaurantId, IOrderRepository repo, IMapper mapper) =>
+app.MapPut("/restaurants/{restaurantId}/orders", [Authorize(Roles = "admin")] async (UpdateOrderContract orderContract, Guid restaurantId, IOrderRepository repo, IMapper mapper) =>
 {
     var order = mapper.Map<Order>(orderContract);
     await repo.Update(restaurantId, order);
     return Results.NoContent();
 });
 
-app.MapDelete("/restaurants/{restaurantId}/orders/{orderId}", async (Guid orderId, Guid restaurantId, IOrderRepository repo) =>
+app.MapDelete("/restaurants/{restaurantId}/orders/{orderId}", [Authorize(Roles = "admin")] async (Guid orderId, Guid restaurantId, IOrderRepository repo) =>
 {
     await repo.Delete(restaurantId,orderId);
     return Results.NoContent();
 });
 
 // Notes
-app.MapGet("/restaurants/{restaurantId}/orders/{orderId}/notes",
-    async (INoteRepository repo, Guid restaurantId, Guid orderId, IMapper mapper) =>
+app.MapGet("/restaurants/{restaurantId}/orders/{orderId}/notes", [Authorize(Roles = "user, admin")]
+async (INoteRepository repo, Guid restaurantId, Guid orderId, IMapper mapper) =>
 {
     var notes = await repo.Get(restaurantId, orderId);
     var noteContracts = mapper.Map<List<NoteContract>>(notes);
     return Results.Ok(noteContracts);
 });
 
-app.MapGet("/restaurants/{restaurantId}/orders/{orderId}/notes/{noteId}", async (Guid noteId, Guid restaurantId, Guid orderId, INoteRepository repo, IMapper mapper) =>
+app.MapGet("/restaurants/{restaurantId}/orders/{orderId}/notes/{noteId}", [Authorize(Roles = "user, admin")] async (Guid noteId, Guid restaurantId, Guid orderId, INoteRepository repo, IMapper mapper) =>
 {
     var note = await repo.Get(restaurantId, orderId, noteId);
     var noteContract = mapper.Map<NoteContract>(note);
     return Results.Ok(noteContract);
 });
 
-app.MapPost("/restaurants/{restaurantId}/orders/{orderId}/notes", async (CreateNoteContract noteContract, Guid restaurantId, Guid orderId, INoteRepository repo, IMapper mapper) =>
+app.MapPost("/restaurants/{restaurantId}/orders/{orderId}/notes", [Authorize(Roles = "admin")] async (CreateNoteContract noteContract, Guid restaurantId, Guid orderId, INoteRepository repo, IMapper mapper) =>
 {
     var note = mapper.Map<Note>(noteContract);
     await repo.Create(restaurantId, orderId, note);
@@ -202,33 +202,33 @@ app.MapPost("/restaurants/{restaurantId}/orders/{orderId}/notes", async (CreateN
     return Results.Created($"/restaurants/{restaurantId}/orders/{orderId}/notes/{noteContractResult.Id}", noteContractResult);
 });
 
-app.MapPut("/restaurants/{restaurantId}/orders/{orderId}/notes", async (UpdateNoteContract noteContract, Guid restaurantId, Guid orderId, INoteRepository repo, IMapper mapper) =>
+app.MapPut("/restaurants/{restaurantId}/orders/{orderId}/notes", [Authorize(Roles = "admin")] async (UpdateNoteContract noteContract, Guid restaurantId, Guid orderId, INoteRepository repo, IMapper mapper) =>
 {
     var note = mapper.Map<Note>(noteContract);
     await repo.Update(restaurantId, orderId, note);
     return Results.NoContent();
 });
 
-app.MapDelete("/restaurants/{restaurantId}/orders/{orderId}/notes/{noteId}", async (Guid noteId, Guid restaurantId, Guid orderId, INoteRepository repo) =>
+app.MapDelete("/restaurants/{restaurantId}/orders/{orderId}/notes/{noteId}", [Authorize(Roles = "admin")] async (Guid noteId, Guid restaurantId, Guid orderId, INoteRepository repo) =>
 {
     await repo.Delete(restaurantId, orderId, noteId);
     return Results.NoContent();
 });
 
 
-app.MapPost("/register", async (CreateUserRequest request, IAuthService authService) =>
+app.MapPost("/register", [AllowAnonymous] async (CreateUserRequest request, IAuthService authService) =>
 {
     var user = await authService.Register(request);
     return Results.Ok(user);
 });
 
-app.MapGet("/users", async (IRepository<User> repo) =>
+app.MapGet("/users", [Authorize(Roles = "admin")] async (IRepository<User> repo) =>
 {
     var users = await repo.GetAll();
     return Results.Ok(users);
 });
 
-app.MapPost("/login", async (UserContract request, IAuthService authService) =>
+app.MapPost("/login", [AllowAnonymous] async (UserContract request, IAuthService authService) =>
 {
     var jwt = await authService.Login(request);
     return Results.Ok(jwt);
