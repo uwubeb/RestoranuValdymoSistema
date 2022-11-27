69d56a79d0cb8c51b5604c9a75e6a90b67988c83
Arūnas Zamalis <8914148+uwubeb@users.noreply.github.com>
2022-11-27 18:40:41 +0200

    create react project

---
+++ rvs-react/src/components/App.js
@@ -0,0 +1,11 @@
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <h2 className="main-header">Restoranų valdymo sistema</h2>
+    </div>
+  );
+}
+
+export default App;
